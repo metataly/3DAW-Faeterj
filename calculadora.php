25df2e2a8731ba221015beaa554636ba41aed374
Nathaly <nathaly.jp03@gmail.com>
2024-09-02 13:23:01 -0300

    novas operações: raiz quadrada, inverter sinal, sen, cos e tan

--- calculadora.php
+++ calculadora.php
@@ -16,8 +16,28 @@ elseif ($oper=="mult") {
     $result=$valor1*$valor2;
     echo "<h2>Resultado da Multiplicação: $result</h2>";
 }
-else {
+elseif ($oper=="div"){
     $result=$valor1/$valor2;
     echo "<h2>Resultado da Divisão: $result</h2>";
+}
+elseif ($oper=="raiz") {
+    $result=sqrt($valor1);
+    echo "<h2>Resultado da Raiz Quadrada: $result</h2>";
+}
+elseif ($oper=="invert") {
+    $result=-$valor1;
+    echo "<h2>Resultado da Inversão de Sinal: $result</h2>";
+}
+elseif ($oper=="sen") {
+    $result=sin(deg2rad($valor1));
+    echo "<h2>Resultado do Seno: $result</h2>";
+}
+elseif ($oper=="cos") {
+    $result=cos(deg2rad($valor1));
+    echo "<h2>Resultado do Cosseno: $result</h2>";
+}
+else {
+    $result=tan(deg2rad($valor1));
+    echo "<h2>Resultado da Tangente: $result</h2>";
 }
 ?>
