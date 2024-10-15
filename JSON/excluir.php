572622e20453869cdb7c781da5c1dce61b83fcac
Nathaly <167140283+metataly@users.noreply.github.com>
2024-10-15 15:40:59 -0300

    correção de erros

--- JSON/excluir.php
+++ JSON/excluir.php
@@ -1,19 +1,22 @@
 <?php
+header('Content-Type: application/json');
+
 $arquivo = "usuario.txt"; 
 $numPergunta = $_POST['numperg'];
 
-    if (file_exists($arquivo)) {
+if (file_exists($arquivo)) {
+    $linhas = file($arquivo);
+    $arquivoHandle = fopen($arquivo, 'w') or die(json_encode(["mensagem" => "ERRO: Não foi possível abrir o arquivo!", "status" => "erro"]));
 
-        $linhas = file($arquivo);
-        $arquivoHandle = fopen($arquivo, 'w') or die( echo json_encode(["mensagem" => "ERRO: Arquivo não existe!", "status" => "erro"]););
-
-        foreach ($linhas as $linha) {
-            $coluna = explode(';', $linha);
-            if (trim($coluna[0]) != $numPergunta) {
-                fwrite($arquivoHandle, $linha);
-            }
+    foreach ($linhas as $linha) {
+        $coluna = explode(';', $linha);
+        if (trim($coluna[0]) != $numPergunta) {
+            fwrite($arquivoHandle, $linha);
         }
-        fclose($arquivoHandle);
-        echo json_encode(["mensagem" => "Excluído com sucesso!", "status" => "sucesso"]);
     }
+    fclose($arquivoHandle);
+    echo json_encode(["mensagem" => "Excluído com sucesso!", "status" => "sucesso"]);
+} else {
+    echo json_encode(["mensagem" => "ERRO: Arquivo não existe!", "status" => "erro"]);
+}
 ?>
